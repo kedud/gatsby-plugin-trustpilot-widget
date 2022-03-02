bccb36daa80ffb7492eba359418c00f1d86e45d0
kedud <vincent.dudek@gmail.com>
2022-03-02 14:43:49 +0100

    Gatsby v4

--- src/hooks/useSitePluginOptions.js
+++ src/hooks/useSitePluginOptions.js
@@ -1,21 +1,18 @@
 import { useStaticQuery, graphql } from "gatsby";
 
 const useSitePluginOptions = () => {
-    const { sitePlugin } = useStaticQuery(
-        graphql`
-                query TrustPilot {
-                        sitePlugin(
-                                name: { eq: "@dadoagency/gatsby-plugin-trustpilot-widget" }
-                            ) {
-                                    pluginOptions {
-                                            username
-                                            business
-                                        }
-                                    }
-                                }
-                            `
-    );
+  const { sitePlugin } = useStaticQuery(
+    graphql`
+      query TrustPilot {
+        sitePlugin(
+          name: { eq: "@dadoagency/gatsby-plugin-trustpilot-widget" }
+        ) {
+          pluginOptions
+        }
+      }
+    `
+  );
 
-    return { ...sitePlugin.pluginOptions }
-}
-export default useSitePluginOptions
+  return { ...sitePlugin.pluginOptions };
+};
+export default useSitePluginOptions;
